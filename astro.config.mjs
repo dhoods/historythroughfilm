7ed7fbbf85dbadff61b71301528e4f3d9627635a
dhoods <hoodds@proton.me>
2024-08-22 18:13:22 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://dhoods.github.io",
+  base: "historythroughfilm",
   trailingSlash: "always",
   integrations: [
     tailwind(),
