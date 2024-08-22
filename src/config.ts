1563bafe5ff2268501b3caeed15db371ae186db9
Darian Samuel Hood <hoodds@proton.me>
2024-08-22 18:31:58 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -47,8 +47,8 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'History Through Film',
+  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Lorem Ipsum',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
